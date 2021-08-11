98b82e6af0455035fa85e89187d2f576e2f5b982
Lucas Randisi <lucasrandisi@gmail.com>
2021-08-11 18:22:33 -0300

    Unprocessable Entity Exception

---
+++ app/Exceptions/UnprocessableEntityException.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class UnprocessableEntityException extends Exception
+{
+    protected int $applicationErrorCode;
+
+    public function __construct($message, int $applicationErrorCode) {
+        parent::__construct($message, 422);
+
+        $this->applicationErrorCode = $applicationErrorCode;
+    }
+
+    public function render()
+    {
+        return response()->json([
+            'applicationErrorCode' => $this->applicationErrorCode,
+            'message' => $this->message
+        ], 422);
+    }
+}
---
+++ error-codes.md
@@ -0,0 +1,3 @@
+Stocks 1XX
+---------------
+- 100: Insufficient finance to buy stocks
