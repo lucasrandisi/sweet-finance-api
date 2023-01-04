28f9d9717a104a8cc4fd0a74637c946d13944c28
Lucas Randisi <lucasrandisi@gmail.com>
2023-01-03 23:54:22 -0300

    Remove unused import

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Exceptions\UnprocessableEntityException;
 use App\Http\Requests\ChangePasswordRequest;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
