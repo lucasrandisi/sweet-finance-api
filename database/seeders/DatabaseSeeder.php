fa0d21cac1232dce3460ea4265b9e5b9aded5a4f
lucasrandisi <lucasrandisi@gmail.com>
2023-09-14 20:50:15 -0300

    Remove seeders call

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-            TwelveDataKeysSeeder::class,
-            MarketauxKeysSeeder::class,
-            FmpApiKeysSeeder::class
-        ]);
+        $this->call([]);
     }
 }
